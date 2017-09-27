9a82f5cee3b8f156d9ad9f744cb2cc32d483994d
LBalkenholUniMelb <lbalkenhol@student.unimelb.edu.au>
2017-09-27 10:00:33 +1000

    Meeting with Christian. Preparation for running MPI code on cloud.

--- cosmologydigitisation/testfunctions.py
+++ cosmologydigitisation/testfunctions.py
@@ -6,32 +6,19 @@ from copy import *
 from cosmdigitclasses import *
 from matplotlib.image import *
 from digitisationschemes import *
+from csv import *
 
-Ar = normal(size = (100, 100))
-Ai = normal(size = (100, 100))
-freqx = fftshift(fftfreq(100, 1))
-freqx2d = tile(freqx, (100, 100))
-freqy = transpose(freqx)
-freqy2d = transpose(tile(freqy, (100, 100)))
+pixelnumber = 512
+file = open("CMBMap.txt")
+cmbmap = zeros((pixelnumber, pixelnumber))
+rowindex = 0
+for row in file:
+    rowvalues = [float(i) for i in row.split()]
+    cmbmap[rowindex] = rowvalues[:pixelnumber]
+    rowindex += 1
 
-cl2d = zeros((100, 100))
-lknee = 2
-for y in range(100):
-    for x in range(100):
-        l = (2*pi*freqx2d[y][x]**2 + 2*pi*freqy2d[y][x]**2)**0.5
-        cl2d[y][x] = (1 + (l/lknee)**(8.0/3.0))
-    print(y)
-print(freqx)
-noisenorm = real(ifft2(Ar + 1j*Ai))
 
-Ar = Ar * cl2d
-Ai = Ai * cl2d
 
-noise = real(ifft2(Ar + 1j*Ai))
-
-c = Cosmologist()
-k, p, perr, hits = c.sriniPowerSpectrum([100, 100, 1, 1], noise)
-k0, p0, perr0, hits0 = c.sriniPowerSpectrum([100, 100, 1, 1], noisenorm)
-plot(k, p, "r")
-plot(k0, p0, "b")
-show()
+wr = writer(open("CMBMapascsv.csv", 'w'))
+for row in cmbmap:
+    wr.writerow(row)
